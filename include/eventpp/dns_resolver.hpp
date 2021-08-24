629b3c013a0ac493c76e7d866b2c0636ad48ea25
msaf1980 <msaf1980@gmail.com>
2021-08-24 11:23:45 +0500

    dns resolver

---
+++ include/eventpp/dns_resolver.hpp
@@ -0,0 +1,51 @@
+#ifndef __EVENTPP_DNS_RESOLVER_HPP__
+#define __EVENTPP_DNS_RESOLVER_HPP__
+
+#include "inner_pre.hpp"
+#include "duration.hpp"
+#include "sys/sys_addrinfo.h"
+
+struct evdns_base;
+struct evdns_getaddrinfo_request;
+namespace eventpp {
+class EventLoop;
+class TimerEventWatcher;
+class EVPP_EXPORT DNSResolver : public std::enable_shared_from_this<DNSResolver> {
+public:
+    //TODO IPv6 DNS resolver
+    typedef std::function<void(const std::vector<struct in_addr>& addrs, int err)> Functor;
+
+    DNSResolver(EventLoop* evloop, const std::string& host, Duration timeout, const Functor& f);
+    ~DNSResolver();
+    void Start();
+    void Cancel();
+    const std::string& host() const {
+        return host_;
+    }
+private:
+    void SyncDNSResolve();
+#if LIBEVENT_VERSION_NUMBER >= 0x02001500    
+    void AsyncDNSResolve();
+#endif
+    void AsyncWait();
+    void OnTimeout();
+    void OnCanceled();
+    void ClearTimer();
+    void OnResolved(int errcode, struct addrinfo* addr);
+    void OnResolved();
+    static void OnResolved(int errcode, struct addrinfo* addr, void* arg);
+
+    EventLoop* loop_;
+    struct evdns_base* dnsbase_;
+    struct evdns_getaddrinfo_request* dns_req_;
+    std::string host_;
+    Duration timeout_;
+    Functor functor_;
+    int err_;
+    std::unique_ptr<TimerEventWatcher> timer_;
+    std::vector<struct in_addr> addrs_;
+};
+
+}
+
+#endif // __EVENTPP_DNS_RESOLVER_HPP__
---
+++ test/dns_resolver_test.cpp
@@ -0,0 +1,110 @@
+#include <eventpp/dns_resolver.hpp>
+#include <eventpp/event_loop.hpp>
+#include <eventpp/event_loop_thread.hpp>
+
+#include <memory>
+
+#include "doctest.h"
+
+TEST_CASE("testDNSResolver")
+{
+    for (int i = 0; i < 6; i++)
+    {
+        std::atomic<int> resolved(0);
+        bool deleted = false;
+        std::vector<struct in_addr> addrs_resolved;
+        int err_resolved = 0;
+
+        auto fn_resolved = [&resolved, &addrs_resolved, &err_resolved](const std::vector<struct in_addr> & addrs, int err) {
+            // LOG_INFO << "Entering fn_resolved";
+            addrs_resolved = addrs;
+            err_resolved = err;
+            resolved.fetch_add(1);
+        };
+
+        eventpp::Duration delay(double(3.0)); // 3s
+        std::unique_ptr<eventpp::EventLoopThread> t(new eventpp::EventLoopThread);
+        t->Start(true);
+        std::shared_ptr<eventpp::DNSResolver> dns_resolver(new eventpp::DNSResolver(t->loop(), "www.google.com", delay, fn_resolved));
+        dns_resolver->Start();
+
+        while (!resolved)
+        {
+            usleep(1);
+        }
+        REQUIRE(err_resolved == 0);
+        REQUIRE(addrs_resolved.size() > 0);
+
+        auto fn_deleter = [&deleted, dns_resolver]() {
+            // LOG_INFO << "Entering fn_deleter";
+            deleted = true;
+        };
+
+        t->loop()->QueueInLoop(fn_deleter);
+        dns_resolver.reset();
+        while (!deleted)
+        {
+            usleep(1);
+        }
+
+        t->Stop(true);
+        t.reset();
+        REQUIRE(eventpp::GetActiveEventCount() == 0);
+    }
+}
+
+TEST_CASE("testDNSResolverTimeout")
+{
+    for (int i = 0; i < 6; i++)
+    {
+        std::atomic<int> resolved(0);
+        bool deleted = false;
+        std::vector<struct in_addr> addrs_resolved;
+        int err_resolved = 0;
+
+        auto fn_resolved = [&resolved, &addrs_resolved, &err_resolved](const std::vector<struct in_addr> & addrs, int err) {
+            std::cerr << "Entering fn_resolved addrs.size=" << addrs.size();
+            addrs_resolved = addrs;
+            err_resolved = err;
+            resolved.fetch_add(1);
+        };
+
+        // 1us to make it timeout immediately
+        eventpp::Duration delay(double(0.000001));
+        std::unique_ptr<eventpp::EventLoopThread> t(new eventpp::EventLoopThread);
+        t->Start(true);
+
+        auto loop = t->loop();
+        std::shared_ptr<eventpp::DNSResolver> dns_resolver(
+            new eventpp::DNSResolver(loop, "wwwwwww.en.cppreference.com", delay, fn_resolved));
+        dns_resolver->Start();
+
+        while (!resolved)
+        {
+            usleep(1);
+        }
+        REQUIRE(err_resolved == EVUTIL_EAI_CANCEL);
+        REQUIRE(addrs_resolved.size() == 0);
+
+        auto fn_deleter = [&deleted, dns_resolver]() {
+            // LOG_INFO << "Entering fn_deleter";
+            deleted = true;
+        };
+
+        loop->QueueInLoop(fn_deleter);
+        dns_resolver.reset();
+        while (!deleted)
+        {
+            usleep(1);
+        }
+
+        loop->RunAfter(eventpp::Duration(0.05), [loop]() { loop->Stop(); });
+        while (!t->IsStopped())
+        {
+            usleep(1);
+        }
+        t.reset();
+        assert(resolved.load() == 1);
+        REQUIRE(eventpp::GetActiveEventCount() == 0);
+    }
+}
